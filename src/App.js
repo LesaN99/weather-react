6cca97050974fe326d4712f86ef9873c64d63996
LesaN99 <149886844+LesaN99@users.noreply.github.com>
2024-02-03 14:54:44 +0200

    Added each files content, imported  bootstrap and then added react animated components.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 
-import './App.css';
 import Weather from "./Weather";
+import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather  />
-      </header>
+      <div className="container">
+    <Weather defaultCity= "Johannesburg" />
+      <footer>
+          This project was coded by
+          <a href="https://github.com/LesaN99" target="_blank" rel="noreferrer"> Palesa Ndlovu </a> and is
+          <a href="https://github.com/LesaN99/react-weather" target="_blank"  rel="noreferrer"> on GitHub</a> and
+          <a href="https://dulcet-fairy-9dab11.netlify.app/" target="_blank"  rel="noreferrer"> hosted on Netlify</a>
+        </footer>
+    </div>
     </div>
   );
 }
-
-export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,90 +1,68 @@
-import axios from "axios";
-import React from "react";
+import React, {useState} from "react";
 import "./Weather.css";
-import { useState} from "react";
+import Weatherinfo from "./Weatherinfo";
+import axios from "axios";
 
 
-export default function Weather() {
-  const [city,setCity] = useState("");
-  const [weather, setWeather] = useState({});
-  const [heading, setHeading] = useState("Johannesburg");
 
+export default function Weather(props) {
+    const [weather, setWeather] = useState({ready: false});
+    const [city,setCity] = useState(props.defaultCity);
 
-function displayWeather(response) {
-    setWeather({
+function showTemperature(response) {
+  setWeather({
+      ready: true,
+      city: response.data.name,
+      date: new Date(response.data.dt * 1000),
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       description: response.data.weather[0].description,
     });
-    setHeading(city);
-    
 }
 
-
-
-function searchCity(event) {
+function handleSubmit(event) {
     event.preventDefault();
-    let apiKey = `616b14cbd38253313b3b8852fa77335d`
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
+    search();
 }
 
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
+function search() {
+  const apiKey= "e450bc345a80a08ada69fd5c714d871d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+     axios.get(apiUrl).then(showTemperature);
+}
 
- 
+function changeCity(event) {
+setCity(event.target.value);
 
-   return (
-    <div className="weather">
-        <div className="weather-app">
-        <header>
-        <form onSubmit={searchCity}>
-          <i class="fa-solid fa-magnifying-glass magnifying-icon"></i>
+}
+
+if (weather.ready) {
+    return (
+        <div className="Weather">
+            <div className="weather-app">
+                <header>
+            <form onSubmit={handleSubmit}>
           <input
             type="search"
             placeholder= "Enter a city.."
             required
             className="search-input"
-            onChange={updateCity}
+            onChange={changeCity}
           />
           <input type="submit" value="Search" className="search-button" />
         </form>
-      </header>
-      <main>
-        <div className="current-weather">
-          <div>
-            <h1 className="current-city">{heading} </h1>
-            <p className="current-details">
-              <span>Wednesday</span>,  <span>{weather.description}</span>
-               <br />
-              Humidity: <strong> {weather.humidity}</strong>, Wind: <strong> {weather.wind}km/h </strong>
-            </p>
-          </div>
-          <div className="current-temperature">
-            <span className="current-temperature-icon"> 
-             <img src={weather.icon} alt={weather.description} />
-            </span>
-            <span className="current-temperature-value"id="temperature">{Math.round(weather.temperature)}</span>
-            <span className="current-temperature-unit">°C</span>
-          </div>
+        <Weatherinfo data={weather}/>
+        </header>
         </div>
-        <div className = "weather-forecast"></div>
-      </main>
-      <footer>
-        <p>
-          This project was coded by
-          <a href="https://github.com/LesaN99" target="_blank" rel="noreferrer">Palesa Ndlovu</a> and is
-          <a href="https://github.com/LesaN99/weather-react" target="_blank"  rel="noreferrer"> on GitHub</a> and
-          <a href="https://dulcet-fairy-9dab11.netlify.app/" target="_blank"  rel="noreferrer"> hosted on Netlify</a>
-        </p>
-      </footer>
-      </div>
-    </div>
-   )
+        </div>
+    );
 
-
-
+} else {
+    search();
+    return "Loading...";
 }
+   
+}
+
