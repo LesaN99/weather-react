814e1d5ab21a10d2d9e6ee3e93383187e15db09d
LesaN99 <149886844+LesaN99@users.noreply.github.com>
2024-02-07 16:35:47 +0200

    Created a new component and added forecast data to be able to display  the daily forecast for 5 days.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,36 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React,{useState} from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
+    let [loaded,setLoaded] = useState(false);
+    let [forecast,setForecast] = useState(null);
+
+
     function handleResponse(response) {
-    console.log(response.data);
-    }
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+    
+    if (loaded) {
+    return (
+        <div className="WeatherForecast">
+            <div className="row">
+                {forecast.map(function(dailyForecast, index){
+                    if (index < 5) {
+                        return (
+                         <div className="col"key={index}>
+                  <WeatherForecastDay data={dailyForecast}/>
+                </div>
+                    );
+                    }
+                })}
+            </div>
+        </div>
+    );
+    } else {
     let apiKey = `58a6775f97527351bf6c6966e209be39`;
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
@@ -14,21 +38,6 @@ export default function WeatherForecast(props) {
 
     axios.get(apiUrl).then(handleResponse);
 
-    return (
-        <div className="WeatherForecast">
-            <div className="row">
-                <div className="col">
-                   <div className="WeatherForecast-day"> Thu </div>
-                   <div className="WeatherForecast-icon">
-                    <WeatherIcon code="01d" size={36} />
-                    </div>
-                     <div className="WeatherForecast-temperature"> 
-                     <span className="WeatherForecast-temperature-max">19°</span>
-                     <span className="WeatherForecast-temperature-min">10°</span>
-                     </div>
-                </div>
-            </div>
-
-        </div>
-    )
+    return null;
+    }
 }
